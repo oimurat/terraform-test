f7aa7964cdd3a70bafc615ae4c90a9b42e8eed56
老村翼 <oimurat@systena.co.jp>
2025-07-24 17:02:46 +0900

    fix:waf

--- waf.tf
+++ waf.tf
@@ -3,24 +3,5 @@ resource "oci_waf_web_app_firewall_policy" "test_web_app_firewall_policy" {
     compartment_id = var.compartment_ocid
 
     #Optional
-    actions {
-        #Required
-        name = "test-action"
-        type = "RETURN_HTTP_RESPONSE"
-
-        #Optional
-        body {
-            #Required
-            text = "Unauthorized"
-            type = "application/json"
-        }
-        code = 401
-        headers {
-
-            #Optional
-            name = "Content-Type"
-            value = "application/json"
-        }
-    }
     display_name = "test-web-app-firewall-policy"
 }
