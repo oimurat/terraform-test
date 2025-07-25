a9c8430728d2825bb262dd67e6cb446a00e73d1f
老村翼 <oimurat@systena.co.jp>
2025-07-25 14:34:31 +0900

    fix:output

--- output.tf
+++ output.tf
@@ -1,47 +1,3 @@
-
-# Outputs for compute instance
-
-# output "public-ip-for-compute-instance" {
-#   value = oci_core_instance.ubuntu_instance.public_ip
-# }
-
-# output "instance-name" {
-#   value = oci_core_instance.ubuntu_instance.display_name
-# }
-
-# output "instance-OCID" {
-#   value = oci_core_instance.ubuntu_instance.id
-# }
-
-# output "instance-region" {
-#   value = oci_core_instance.ubuntu_instance.region
-# }
-
-# output "instance-shape" {
-#   value = oci_core_instance.ubuntu_instance.shape
-# }
-
-# output "instance-state" {
-#   value = oci_core_instance.ubuntu_instance.state
-# }
-
-# output "instance-OCPUs" {
-#   value = oci_core_instance.ubuntu_instance.shape_config[0].ocpus
-# }
-
-# output "instance-memory-in-GBs" {
-#   value = oci_core_instance.ubuntu_instance.shape_config[0].memory_in_gbs
-# }
-
-# output "time-created" {
-#   value = oci_core_instance.ubuntu_instance.time_created
-# }
-
-# The "name" of the availability domain to be used for the compute instance.
-output "name-of-first-availability-domain" {
-  value = data.oci_identity_availability_domains.ads.availability_domains[0].name
-}
-
 output "web-app-firewall-policy-OCID" {
   value = oci_waf_web_app_firewall_policy.test_web_app_firewall_policy.id
 }
@@ -64,4 +20,12 @@ output "web-app-firewall-OCID" {
 
 output "web-app-firewall-name" {
   value = oci_waf_web_app_firewall.test_web_app_firewall.display_name
+}
+
+output "web-app-firewall-state" {
+  value = oci_waf_web_app_firewall.test_web_app_firewall.state
+}
+
+output "web-app-firewall-time-created" {
+  value = oci_waf_web_app_firewall.test_web_app_firewall.time_created
 }
