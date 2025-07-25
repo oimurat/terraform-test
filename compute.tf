6d3ef6fad75c0b2c143d60f31eff951020074a8f
老村翼 <oimurat@systena.co.jp>
2025-07-25 13:47:47 +0900

    test:multi resource

--- compute.tf
+++ compute.tf
@@ -1,18 +1,18 @@
-# resource "oci_core_instance" "ubuntu_instance" {
-#     # Required
-#     availability_domain = data.oci_identity_availability_domains.ads.availability_domains[0].name
-#     compartment_id = var.compartment_ocid
-#     shape = "VM.Standard2.1"
-#     source_details {
-#         source_id = var.instance_image_ocid
-#         source_type = "image"
-#     }
+resource "oci_core_instance" "ubuntu_instance" {
+    # Required
+    availability_domain = data.oci_identity_availability_domains.ads.availability_domains[0].name
+    compartment_id = var.compartment_ocid
+    shape = "VM.Standard2.1"
+    source_details {
+        source_id = var.instance_image_ocid
+        source_type = "image"
+    }
 
-#     # Optional
-#     display_name = "terraform-instance"
-#     create_vnic_details {
-#         assign_public_ip = true
-#         subnet_id = var.subnet_ocid
-#     }
-#     preserve_boot_volume = false
-# }
+    # Optional
+    display_name = "terraform-instance"
+    create_vnic_details {
+        assign_public_ip = true
+        subnet_id = var.subnet_ocid
+    }
+    preserve_boot_volume = false
+}
