69a84c5d15016e66947d34ff7f9adfb76ae7f5be
老村翼 <oimurat@systena.co.jp>
2025-08-01 17:24:10 +0900

    delete:ocir,email

--- ocir.tf
+++
@@ -1,7 +0,0 @@
-module "registry_test" {
-    source = "web-virtua-oci-multi-account-modules/container-registry/oci"
-    version = "1.0.0"
-
-    name           = "terraform-test-registry"
-    compartment_id = var.testing_compartment_ocid
-}
